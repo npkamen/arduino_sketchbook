619c804f28763700a437ef2d2cb142190540f320
npkamen <nick@kamenyitzky.com.au>
2016-02-20 16:52:07 +1100

    Added project files

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,113 @@
+ #include <ELECHOUSE_CC1101.h>
+ 
+ void setup()
+{
+  Serial.begin(9600);
+  ELECHOUSE_cc1101.Init();
+  ELECHOUSE_cc1101.SetReceive();
+}
+
+byte RX_buffer[61]={0};
+byte RX_rssi=0;
+byte RX_lqi=0;
+byte size,i,flag;
+
+float battv=0.0;
+long lastMsgTm=0;
+long lastPrintTm=0;
+long currentTm=0;
+int rssi=0;
+int quality=0;
+boolean msgRec=false;
+
+void loop()
+{
+  
+  currentTm=millis();
+  
+  if(ELECHOUSE_cc1101.CheckReceiveFlag())
+  {
+    msgRec=true;
+    size=ELECHOUSE_cc1101.ReceiveData(RX_buffer, RX_rssi, RX_lqi);
+    //Serial.print("RawRX: ");
+    //for(i=0;i<size;i++)
+    //{
+    //  Serial.print(RX_buffer[i],DEC);
+    //  Serial.print(' ');
+    //}
+    
+    //Serial.println("");
+    
+    //convert RSSI to actual figure
+    if(RX_rssi>=128)
+    {
+      rssi=(RX_rssi-256)/2-74;
+    }
+    else
+    {
+      rssi=(RX_rssi/2)-74;
+    }
+
+    //scale LQI to 0-100% quality figure
+    quality=(100 - ((RX_lqi - 128)/128*100));
+    
+    //convert battery voltage back to int
+    battv=float(RX_buffer[4]*256 + RX_buffer[5])/1000.0;
+    
+    ELECHOUSE_cc1101.SetReceive();
+    
+    lastMsgTm=currentTm;
+  }
+  
+  //if(((currentTm - lastPrintTm) > 10000) || (msgRec))
+  if(Serial.available() > 0)
+  {
+    msgRec=false;
+  
+    if(lastMsgTm==0)
+    {
+      Serial.println("Waiting for first data message...");
+    }
+    else
+    {
+      if (Serial.read() == char('s'))
+      {
+        
+        Serial.print("Status - Level:");
+        Serial.print(RX_buffer[3]);
+        Serial.print("% Sensor Distance:");
+        Serial.print(RX_buffer[1]*256 + RX_buffer[2]);
+        Serial.print("cm Signal:");
+        Serial.print(rssi);
+        Serial.print("dBm Quality:");
+        Serial.print(quality);    
+        Serial.print("% Battery Volts:");
+        Serial.print(battv);
+        Serial.print("V Last Message Received:");
+        Serial.print((currentTm-lastMsgTm)/1000);
+        Serial.println("s ago.");
+      }
+      else
+      {
+        Serial.print("Level:");
+        Serial.print(RX_buffer[3]);
+        Serial.print(" RSSI:");
+        Serial.print(rssi);
+        Serial.print(" Quality:");
+        Serial.print(quality);
+        Serial.print(" BattV:");
+        Serial.print(battv);
+        Serial.println("");
+      }
+    }
+    lastPrintTm = millis();
+    
+    //Clear serial buffer if something new comes in
+    while(Serial.available() > 0)
+    {
+      char t = Serial.read();
+    }
+    
+  }
+}
+
---
+++ Transmitter/Transmitter.ino
@@ -0,0 +1,90 @@
+#include <ELECHOUSE_CC1101.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+//Set up sensor distance parameters
+#define MAX_DISTANCE 300
+#define TANK_HEIGHT 257.0
+#define MIN_DISTANCE 20.0
+
+#define size 6
+
+byte TX_buffer[size]={0};
+int i=0;
+int battv=0;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup()
+{
+  //Serial.begin(9600);
+  ELECHOUSE_cc1101.Init();
+ }
+
+void loop()
+{
+  //Ping and condition tank level
+  int uS = sonar.ping_median(30);
+  int dist = uS / US_ROUNDTRIP_CM;
+  float lvlpf = (TANK_HEIGHT - dist + MIN_DISTANCE) / TANK_HEIGHT * 100.0;
+
+  if (lvlpf < 0.0) {
+    lvlpf = 0.0;
+  }
+  else if (lvlpf > 100.0) {
+    lvlpf = 100.0;
+  }
+  int lvlp = lvlpf; 
+  
+  battv=readVcc();
+  
+  Serial.print(lvlp);
+  Serial.print(' ');
+  Serial.print(highByte(battv));
+  Serial.print(' ');
+  Serial.println(lowByte(battv));
+  
+  //Map data to array to be transmitted
+  TX_buffer[0]=i;
+  TX_buffer[1]=highByte(dist);
+  TX_buffer[2]=lowByte(dist);
+  TX_buffer[3]=lvlp;
+  TX_buffer[4]=highByte(battv);
+  TX_buffer[5]=lowByte(battv);
+  
+  //Send data
+  ELECHOUSE_cc1101.SendData(TX_buffer,size);
+  
+  i++;
+  
+  //Wait for 60s before re-sampling and sending again
+  delay(60000);
+}
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1357400L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
