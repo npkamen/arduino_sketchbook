d6c076ccd1a440185f5db7a72ab682054d05548e
npkamen <nick@kamenyitzky.com.au>
2016-02-23 16:39:59 +1100

    Changed internal ref constant back to default
    Misconception between guide read and what the functionality actually does.  Initially adjusted to be suitable for supply battery voltage.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -85,6 +85,6 @@ long readVcc() {
 
   long result = (high<<8) | low;
 
-  result = 1357400L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
   return result; // Vcc in millivolts
 }
