a55ff4cfa5e2dbae3030b43e06b2fc0e260b2966
Nick Kamenyitzky <nick@kamenyitzky.com.au>
2016-05-30 15:13:25 +1000

    Renamed tank level projects to allow for greater scope of sketchbook

---
+++ tanklvl_receiver/tanklvl_receiver.ino
@@ -0,0 +1,128 @@
+ #include <ELECHOUSE_CC1101.h>
+ 
+ void setup()
+{
+  Serial.begin(9600);
+  ELECHOUSE_cc1101.Init();
+  ELECHOUSE_cc1101.SetReceive();
+}
+
+byte RX_buffer[61]={0};
+byte RX_rssi=0;
+byte RX_lqi=0;
+byte size,i,flag;
+
+float battv=0.0;
+float tankpv=0.0;
+int tankcm=0;
+long lastMsgTm=0;
+long lastPrintTm=0;
+long currentTm=0;
+float rssi=0;
+int quality=0;
+boolean msgRec=false;
+
+void loop()
+{
+  
+  currentTm=millis();
+  
+  if(ELECHOUSE_cc1101.CheckReceiveFlag())
+  {
+    msgRec=true;
+    size=ELECHOUSE_cc1101.ReceiveData(RX_buffer, RX_rssi, RX_lqi);
+
+    //convert RSSI to actual figure
+    if(RX_rssi>=128)
+    {
+      rssi=(float(RX_rssi-256)/2.0)-74.0;
+    }
+    else
+    {
+      rssi=(float(RX_rssi)/2.0)-74.0;
+    }
+
+    //condition tank level in cm coming back from tank sensor
+    tankcm = RX_buffer[1]*256+RX_buffer[2];
+    if (tankcm < 20)
+    {
+      tankcm = 20;
+    }
+
+    //convert incoming cm reading to float %
+    tankpv = float(257 - tankcm) / 227.0 * 100.0;
+    if (tankpv < 0.0)
+    {
+     tankpv = 0.0;
+    }
+    else if (tankpv > 100.0)
+    {
+     tankpv = 100.0;
+  }
+
+    //scale LQI to 0-100% quality figure
+    quality=(100 - ((RX_lqi - 128)/128*100));
+    
+    //convert battery voltage back to int
+    battv=float(RX_buffer[4]*256 + RX_buffer[5])/1000.0;
+    
+    ELECHOUSE_cc1101.SetReceive();
+    
+    lastMsgTm=currentTm;
+  }
+  
+  if(Serial.available() > 0)
+  {
+    char cmd=Serial.read();
+    msgRec=false;
+  
+    if(lastMsgTm==0)
+    {
+      Serial.println("Waiting for first data message...");
+    }
+    else if (((currentTm-lastMsgTm)/1000) > 300)
+    {
+      Serial.println("Comms Failure with Sensor");
+    }
+    else
+    {
+      if (cmd == char('s'))
+      {
+        
+        Serial.print("Status - Level:");
+        Serial.print(tankpv);
+        Serial.print("% Sensor Distance:");
+        Serial.print(RX_buffer[1]*256 + RX_buffer[2]);
+        Serial.print("cm Signal:");
+        Serial.print(rssi);
+        Serial.print("dBm Quality:");
+        Serial.print(quality);    
+        Serial.print("% Battery Volts:");
+        Serial.print(battv);
+        Serial.print("V Last Message Received:");
+        Serial.print((currentTm-lastMsgTm)/1000);
+        Serial.println("s ago.");
+      }
+      else if (cmd == char('c'))
+      {
+        Serial.print("Level:");
+        Serial.print(tankpv);
+        Serial.print(" RSSI:");
+        Serial.print(rssi);
+        Serial.print(" Quality:");
+        Serial.print(quality);
+        Serial.print(" BattV:");
+        Serial.print(battv);
+        Serial.println("");
+      }
+    }
+    
+    //Clear serial buffer if something new comes in during serial transfer of data
+    while(Serial.available() > 0)
+    {
+      char t = Serial.read();
+    }
+    
+  }
+}
+
---
+++ tanklvl_transmitter/tanklvl_transmitter.ino
@@ -0,0 +1,106 @@
+#include <ELECHOUSE_CC1101.h>
+#include <NewPing.h>
+#include <JeeLib.h> // Low power functions library
+
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+//Set up sensor distance parameters
+#define MAX_DISTANCE 300 //Distance for sensor to give up read if no response received by
+#define TANK_HEIGHT 257 //Distance from sensor face to bottom of tank
+#define MIN_DISTANCE 30 //Distance from sensor face to minimum measurement distance
+
+#define size 6
+
+byte TX_buffer[size]={0};
+int i=0;
+int battv=0;
+
+int lvlpwrPin = 4;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Setup the watchdog
+
+
+void setup()
+{
+  Serial.begin(9600);
+  ELECHOUSE_cc1101.Init();
+  pinMode(lvlpwrPin, OUTPUT);
+ }
+
+void loop()
+{
+  //Ping and condition tank level
+  digitalWrite(lvlpwrPin, HIGH);
+  delay(1000);
+  int uS = sonar.ping_median(30);
+  
+  digitalWrite(lvlpwrPin, LOW);
+  
+  int dist = uS / US_ROUNDTRIP_CM;
+  float lvlpf = float(TANK_HEIGHT - dist) / float(TANK_HEIGHT - MIN_DISTANCE) * 100.0;
+
+  if (lvlpf < 0.0) {
+    lvlpf = 0.0;
+  }
+  else if (lvlpf > 100.0) {
+    lvlpf = 100.0;
+  }
+  
+  byte lvlp = lvlpf; 
+  
+  battv=readVcc();
+  
+  Serial.print(lvlp);
+  Serial.print(' ');
+  Serial.print(highByte(battv));
+  Serial.print(' ');
+  Serial.println(lowByte(battv));
+  
+  //Map data to array to be transmitted
+  TX_buffer[0]=i;
+  TX_buffer[1]=highByte(dist);
+  TX_buffer[2]=lowByte(dist);
+  TX_buffer[3]=lvlp;
+  TX_buffer[4]=highByte(battv);
+  TX_buffer[5]=lowByte(battv);
+  
+  //Send data
+  ELECHOUSE_cc1101.SendData(TX_buffer,size);
+  
+  i++;
+  
+  delay(1000);
+  
+  //Wait for 60s before re-sampling and sending again
+  //delay(5000);
+  Sleepy::loseSomeTime(60000);
+}
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
